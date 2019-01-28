dd8593d80cf26862a2163e8c20db55153cd137b3
CAMHub PublishBot <octravis@us.ibm.com>
2019-01-28 19:44:08 +0000

    Automated CAMHub::publish on 2019-01-28 19:44:08 UTC

---
+++ templates/icp-ce-minimal/outputs.tf
@@ -0,0 +1,23 @@
+output "ibm_cloud_private_master_ip" {
+  value = "https://${element(ibm_compute_vm_instance.icp-master.*.ipv4_address, 0)}"
+}
+
+output "ICP Console URL" {
+  value = "https://${element(ibm_compute_vm_instance.icp-master.*.ipv4_address, 0)}:8443"
+}
+
+output "ICP Proxy" {
+  value = "${element(ibm_compute_vm_instance.icp-proxy.*.ipv4_address, 0)}"
+}
+
+output "ICP Kubernetes API URL" {
+  value = "https://${element(ibm_compute_vm_instance.icp-master.*.ipv4_address, 0)}:8001"
+}
+
+output "ICP Admin Username" {
+  value = "admin"
+}
+
+output "ICP Admin Password" {
+  value = "${local.icppassword}"
+}
