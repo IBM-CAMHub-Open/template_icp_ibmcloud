ab5a59e831f5ab6907495128cb929035b08ba7da
CAMHub PublishBot <octravis@us.ibm.com>
2019-01-31 15:25:32 +0000

    Automated CAMHub::publish on 2019-01-31 15:25:32 UTC

--- templates/icp-ce-minimal/outputs.tf
+++ templates/icp-ce-minimal/outputs.tf
@@ -6,16 +6,16 @@ output "ibm_cloud_private_master_ip" {
   value = "${element(ibm_compute_vm_instance.icp-master.*.ipv4_address, 0)}"
 }
 
-output "ICP Console URL" {
+output "ibm_cloud_private_console_url" {
   value = "https://${element(ibm_compute_vm_instance.icp-master.*.ipv4_address, 0)}:8443"
+}
+
+output "ibm_cloud_private_kubernetes_api_url" {
+  value = "https://${element(ibm_compute_vm_instance.icp-master.*.ipv4_address, 0)}:8001"
 }
 
 output "ICP Proxy" {
   value = "${element(ibm_compute_vm_instance.icp-proxy.*.ipv4_address, 0)}"
-}
-
-output "ICP Kubernetes API URL" {
-  value = "https://${element(ibm_compute_vm_instance.icp-master.*.ipv4_address, 0)}:8001"
 }
 
 output "ICP Admin Username" {
--- templates/icp-ee/outputs.tf
+++ templates/icp-ee/outputs.tf
@@ -6,24 +6,23 @@ output "ibm_cloud_private_master_ip" {
   value = "${element(ibm_compute_vm_instance.icp-master.*.ipv4_address, 0)}"
 }
 
+output "ibm_cloud_private_console_url" {
+  value = "https://${ibm_lbaas.master-lbaas.vip}:8443"
+}
+
+output "ibm_cloud_private_kubernetes_api_url" {
+  value = "https://${ibm_lbaas.master-lbaas.vip}:8001"
+}
+
+output "ibm_cloud_private_registry_url" {
+  value = "${ibm_lbaas.master-lbaas.vip}:8500"
+}
 output "ICP Console load balancer DNS (external)" {
   value = "${ibm_lbaas.master-lbaas.vip}"
 }
 
 output "ICP Proxy load balancer DNS (external)" {
   value = "${ibm_lbaas.proxy-lbaas.vip}"
-}
-
-output "ICP Console URL" {
-  value = "https://${ibm_lbaas.master-lbaas.vip}:8443"
-}
-
-output "ICP Registry URL" {
-  value = "${ibm_lbaas.master-lbaas.vip}:8500"
-}
-
-output "ICP Kubernetes API URL" {
-  value = "https://${ibm_lbaas.master-lbaas.vip}:8001"
 }
 
 output "ICP Admin Username" {
